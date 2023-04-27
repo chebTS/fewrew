7cc3d8b51416e639471a57267d1ec255f3357242
Cheb <chebts@gmail.com>
2023-04-27 21:11:00 +0300

    https request added

--- bin/ekreative.dart
+++ bin/ekreative.dart
@@ -1,6 +1,38 @@
-import 'package:ekreative/ekreative.dart' as ekreative;
+//import 'package:ekreative/ekreative.dart' as ekreative;
 
-void main(List<String> arguments) {
-  print('Hello world: ${ekreative.calculate()}!');
+import 'dart:convert' as convert;
 
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) async {
+  // This example uses the Google Books API to search
+  // for books about HTTP. For details, see
+  // https://developers.google.com/books/docs/overview
+  //var uri = Uri.https('example.org', '/path', {'q': 'dart'});
+  final url = Uri.https('api.genderize.io','/',{'name':'lena'});
+  // final url = Uri.https(
+  //   'www.googleapis.com',
+  //   '/books/v1/volumes',
+  //   {'q': '{http}'},
+  // );
+
+  // Await the HTTP GET response, then decode the
+  // JSON data it contains.
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse);
+    // final itemCount = jsonResponse['totalItems'];
+    // print('Number of books about HTTP: $itemCount.');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
 }
+
+
+//
+// void main(List<String> arguments) {
+//   print('Hello world: ${ekreative.calculate()}!');
+//
+//}
